3f6906342a7c37d842a39d428a256bdbc3dbbc36
gideonn12 <gideoneeman@icloud.com>
2024-01-26 15:24:18 +0200

    loops 5 time

--- main.s
+++ main.s
@@ -14,6 +14,8 @@ scanf_fmt:
     .string "%d"
 result_fmt:
     .string "the seed: %d\n"
+guess_fmt:
+    .string "What is your guess? "
 
 .section .text
 .globl main
@@ -34,12 +36,28 @@ main:
     xorq %rax, %rax
     call scanf
 
-    # Print sead
-    movq $result_fmt, %rdi
-    movq user_seed, %rsi
-    xorq %rax, %rax
-    call printf
+    # send seed to srand
+    # movq $user_seed, %rdi
+    # xorq %rax, %rax
+    # call srand
 
+    # i=1 to 5 for loop
+    movq $1, %r12
+    movq $5, %r13
+    jmp loop_five
+
+
+loop_five:
+    cmp %r12,%r13
+    jl exit
+
+    movq  $guess_fmt, %rdi
+    xorq  %rax, %rax
+    call printf
+    inc %r12
+    jmp loop_five
+
+exit:
     # Exit
     xorq %rax, %rax
     movq %rbp, %rsp
